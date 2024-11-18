ef979ed082066bfb714a19a03ac194e804cf73e4
Michele Capichioni <capimichi@gmail.com>
2024-11-18 18:26:15 +0100

    Added dependency injection for Radarr API key and base URL in RadarrClient class

--- radarrsonarrconverter/client/RadarrClient.py
+++ radarrsonarrconverter/client/RadarrClient.py
@@ -1,9 +1,13 @@
+from radarrsonarrconverter.variable.RadarrApiKeyVariable import RadarrApiKeyVariable
+from radarrsonarrconverter.variable.RadarrBaseUrlVariable import RadarrBaseUrlVariable
 from injector import inject
 
 
 class RadarrClient:
+    radarr_base_url: RadarrBaseUrlVariable
+    radarr_api_key: RadarrApiKeyVariable
 
     @inject
-    def __init__(self):
-        pass
-
+    def __init__(self, radarr_base_url: RadarrBaseUrlVariable, radarr_api_key: RadarrApiKeyVariable):
+        self.radarr_base_url = radarr_base_url
+        self.radarr_api_key = radarr_api_key
